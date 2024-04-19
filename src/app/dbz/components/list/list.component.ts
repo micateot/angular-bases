5fade0129543bfa57cad9168f3b959db90b5ddc1
MicaTeot <micaelat@itcsoluciones.com>
2024-04-19 17:09:34 -0300

    Video 63 - Using Input() decorator

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
-export class ListComponent { }
+export class ListComponent {
+  @Input() characterList: Character[] = [
+      {
+        name: 'Trunks',
+        power: 10
+      }
+    ]
+}
