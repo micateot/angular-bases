bf8f0bae333d5a594fe79899a486fd787cb8e15d
MicaTeot <micaelat@itcsoluciones.com>
2024-04-23 16:55:22 -0300

    Video 71 - Services

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,39 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: 'main-page.component.html'
 })
 
-export class MainPageComponent implements OnInit {
+export class MainPageComponent {
 
-  public characters: Character[] = [
-    {
-      name: 'Krillin',
-      power: 1000
-    },
-    {
-      name: 'Goku',
-      power: 9500
-    },
-    {
-      name: 'Vegeta',
-      power: 7500
-    }
-  ];
-
-  constructor() { }
-
-  ngOnInit() { }
-
-  onNewCharacter(character: Character): void {
-    this.characters.push(character);
-    // this.characters = [...this.characters, character]
-  }
-
-  onDeletedCharacter(index: number): void {
-    this.characters.splice(index, 1);
-  }
+  constructor(public dbzService: DbzService){}
 
 }
